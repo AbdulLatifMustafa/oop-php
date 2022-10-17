9b3f7254278e03af1db68e10f7ad4ac52fb5660c
AbdulLatifMustafa <99658332+AbdulLatifMustafa@users.noreply.github.com>
2022-10-17 08:22:26 +0200

    Update Encapsulation.php

--- Encapsulation.php
+++ Encapsulation.php
@@ -1,33 +1,67 @@
 <?php
-class info {
-public function sets($usm,$names,$ids,$hashs,$pas)
+
+trait message
 {
- $this->user=$usm;
- $this->name=$names;
- $this->id=$ids;
- $this->hash=$hashs;
- $this->password=$pas;
- return $this;
+    public function send_message($num)
+    {
+        $this->send_message += $num;
+        return $this;
+    }
 }
-public function getform()
+
+trait message1
 {
-    return $this->sets("xuser","admin",sha1("1"),bin2hex(random_bytes(18)),sha1("mypass"));
+    public function send_message1($num)
+    {
+        $this->send_message *= $num;
+        return $this;
+    }
 }
-private $name;
-private $id;
-private $user;
-private $hash;
-private $password;
+trait message2
+{
+    public function send_message2()
+    {
+
+        return $this->send_message;
+    }
 }
-$um=new info();
+
+class allmessage
+{
+    use message, message1, message2;
+    protected $send_message = 0;
+}
+
+
+
+class info extends allmessage
+{
+    public function sets($usm, $names, $ids, $hashs, $pas)
+    {
+        $this->user = $usm;
+        $this->name = $names;
+        $this->id = $ids;
+        $this->hash = $hashs;
+        $this->password = $pas;
+        return $this;
+    }
+    public function getform()
+    {
+        return $this->sets("xuser", "admin", sha1("1"), bin2hex(random_bytes(18)), sha1("mypass"));
+    }
+
+
+
+
+    private $name;
+    private $id;
+    private $user;
+    private $hash;
+    private $password;
+}
+$um = new info();
+$um->send_message(10)->send_message1(5)->send_message2(0);
 $um->getform();
-echo"<pre>";
-print_r($um) ;
-echo"</pre>";
-
-
-
-
-
-
-
+echo "<pre>";
+print_r($um);
+echo "</pre>";
